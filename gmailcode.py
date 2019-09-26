b193a538887a7b5a47de447284b02c4316471c92
hvu815 <38333072+hvu815@users.noreply.github.com>
2019-09-26 08:46:11 -0700

    Update gmailcode.py
    Update code with better break down of google flight 's email structure

--- gmailcode.py
+++ gmailcode.py
@@ -1,4 +1,9 @@
-from apiclient.discovery import build
+'''
+https://github.com/abhishekchhibber/Gmail-Api-through-Python
+'''
+
+
+from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 import datefinder
@@ -12,7 +17,7 @@ from datetime import datetime as dt
 
 import base64
 import email
-from apiclient import errors
+from googleapiclient import errors
 
 
 args = tools.argparser.parse_args()
@@ -32,7 +37,7 @@ service = build('gmail', 'v1', http=creds.authorize(Http()))
 # calll the Gmail API, only get 1 of the recent message ids
 # First get the message if for the message
 results = service.users().messages().list(userId='me',
-                                          maxResults=10,  # max record to  obtain
+                                          maxResults=20,  # max record to  obtain
                                           q='from: noreply-travel@google.com label:inbox ').execute()  # include filter for message
 
 time = []
@@ -40,103 +45,85 @@ price_now = []
 price_before = []
 airlines = []
 
+
+dollar = re.compile(r'\$\d+\,*\d+')
+eva = re.compile(r'EVA')
+
 for i in range(results['resultSizeEstimate']):
     # get the message id from the results object
     message_id = results['messages'][i]['id']
 
     # use the message id to get the actual message, including any attachments
     message = service.users().messages().get(userId='me', id=message_id).execute()
+    
+    ''' 
+    we know the structure of a message variable and the information we need is from ['snippet']
+    and ['payload']['headers'], so we can directly go there without a loop.
+    Subject and Date are under ['payload']['headers']; however, the object is a list of dictionaries,
+    so we cant reference by name. So, we have to use a loop to go through.
+    '''
 
-    for key in message:
-        if key == 'snippet':
-            # # get line that contains prices info
-            # snippet_index = [m.start() for m in re.finditer('\$', message[key])]
+    '''
+    There are two types of message. One gives prices of multiple airlines and one gives prices of EVA Airlines.
+    This is because the option I chose when I started tracking prices.
+    Eva airlines will have prices in the subject while other emails will have prices in the snippet.
+    '''
+    prices = [p for p in dollar.finditer(message['snippet'])]
+    if prices: 
+        '''
+        if prices are in the snippet, then we will look for airlines as well.
+        There are some exceptions where snippet is cut off without airlines.
+        '''
+        price_now.append(prices[0].group())
+        price_before.append(prices[1].group())
+        spaces = [s for s in re.finditer('\s',message['snippet'][(prices[1].end() + 1):])]
+        airlines.append(message['snippet'][(prices[1].end() + 1):][:(spaces[1].end()-1)])
+        for dic in message['payload']['headers']:
+            if dic['name'] == 'Date':
+                date = dt.strptime(dic['value'], '%a, %d %b %Y %H:%M:%S %z').strftime('%m/%d/%y')
+                time.append(date)
+    else: 
+        tracked_flight = re.search('tracked flight', message['snippet'])
+        spaces = [s for s in re.finditer('\s', message['snippet'][(tracked_flight.end() + 1):])]
+        airlines.append(message['snippet'][(tracked_flight.end()+1):][:(spaces[1].end() - 1)])
+        for dic in message['payload']['headers']:
+            if dic['name'] == 'Subject':
+                prices = [p for p in dollar.finditer(dic['value'])]
+                price_now.append(prices[0].group())
+                price_before.append(prices[1].group())
+            if dic['name'] == 'Date':
+                date = dt.strptime(dic['value'], '%a, %d %b %Y %H:%M:%S %z').strftime('%m/%d/%y')
+                time.append(date)
 
-            # # get prices from string
-            # price_1 = message[key][(snippet_index[0]+1):(snippet_index[1] - 1)]
-            # price_2 = message[key][(snippet_index[1] + 1) : (snippet_index[1] + 5)]
-
-            # try:
-            #     price_now.append(int(price_1))
-            # except:
-            #     price_now.append(price_1)
-            
-            # try:
-            #     price_before.append(int(price_2))
-            # except:
-            #     price_before.append(price_2)
-
-            # to get airline, first get position of colons:
-            colon_index = [m.start() for m in re.finditer('\:', message[key])] #first colon is fixed, always go back from second colon
-            # get posistion of word 'adult'
-            adult_index = message[key].find('adult')
-            # select a string from adult word to the second colon
-            selected_string = message[key][adult_index:colon_index[1]].split()
-            # getting two words before the last word 
-            airline_name = selected_string[len(selected_string) - 3] + ' ' + selected_string[len(selected_string) - 2]
-            airlines.append(airline_name)
-
-        if key == 'payload':
-            for name in message[key]:
-                if name == 'headers':
-                    for i in range(len(message[key][name])):
-                        if message[key][name][i]['name'] == 'Subject':
-                            # find '$' position
-                            subject = message[key][name][i]['value']
-                            price_indexes = [m.start() for m in re.finditer('\$', subject)]
-                            # find '(' position
-                            open_bracket_index = subject.find('(')
-                            close_bracket_index = subject.find(')')
-                            # selecting price
-                            price_1 = subject[(price_indexes[0]+1):(open_bracket_index - 1)]
-                            price_2 = subject[(price_indexes[1]+1):(close_bracket_index)]
-                            price_now.append(price_1)
-                            price_before.append(price_2)
-                        if message[key][name][i]['name'] == 'Date':
-                            # convert to date 
-                            date = dt.strptime(message['payload']['headers'][i]['value'], '%a, %d %b %Y %H:%M:%S %z').strftime('%m/%d/%y')
-                            time.append(date)
-
-
-    # # get line that contains received date of email
-    # date = message['payload']['headers'][2]['value']
-    # # find date from the string
-    # matches = datefinder.find_dates(date, strict=True)
-    # # convert to time format
-    # for match in matches:
-    #     time.append(match.strftime('%m/%d/%Y'))
-
-    # # get line that contains prices info
-    # snippet_index = [m.start() for m in re.finditer('\$', message['snippet'])]
-    # # get prices from string
-    # price_now.append(message['snippet'][(snippet_index[0] + 1):(snippet_index[0] + 5)])
-    # price_before.append(message['snippet'][(snippet_index[1] + 1):(snippet_index[1] + 5)])
 
 # reorder data
 time.reverse()
 price_before.reverse()
 price_now.reverse()
+airlines.reverse()
 
 # bring result into data frame
 data = pd.DataFrame(data={'time': time,
                           'price_now': price_now,
-                          'price_before': price_before})
+                          'price_before': price_before,
+                          'airline': airlines})
 
 # open excel
 workbook = openpyxl.load_workbook('C:\\Users\\huong.vu\\Desktop\\Google-Flights-Prices\\gmail_flight.xlsx')
 # getting sheet in excel
-sheet = workbook.get_sheet_by_name('Sheet1')
+sheet = workbook['Sheet1']
 # getting last row in excel sheet
 last_row = sheet.max_row
 
 # getting historical data
 hist_data = pd.DataFrame(data={'time': [sheet.cell(row=d, column=1).value for d in range(2, last_row + 1)],
                                'price_now': [sheet.cell(row=d, column=2).value for d in range(2, last_row + 1)],
-                               'price_before': [sheet.cell(row=d, column=3).value for d in range(2, last_row + 1)]})
+                               'price_before': [sheet.cell(row=d, column=3).value for d in range(2, last_row + 1)],
+                               'airline': [sheet.cell(row=d, column=4).value for d in range(2, last_row + 1)]})
 
 # remove duplicates
 insert_data = data.join(hist_data, lsuffix='_new', rsuffix='_hist')
-insert_data = insert_data[pd.isnull(insert_data['time_hist'])].iloc[:, 0:3]
+insert_data = insert_data[pd.isnull(insert_data['time_hist'])].iloc[:, 0:4]
 
 # write data into excel file
 for k in range(last_row + 1, len(insert_data) + last_row + 1):
@@ -144,22 +131,9 @@ for k in range(last_row + 1, len(insert_data) + last_row + 1):
         sheet.cell(row=k, column=1).value = insert_data['time_new'].iloc[k - last_row - 1]
         sheet.cell(row=k, column=2).value = insert_data['price_now_new'].iloc[k - last_row - 1]
         sheet.cell(row=k, column=3).value = insert_data['price_before_new'].iloc[k - last_row - 1]
+        sheet.cell(row=k, column=4).value = insert_data['airline_new'].iloc[k - last_row - 1]
     except Exception as e:
         print(str(e))
 
 workbook.save('C:\\Users\\huong.vu\\Desktop\\Google-Flights-Prices\\gmail_flight.xlsx')
 workbook.close()
-
-
-message = service.users().messages().get(userId='me', id='16cfd5f696777359', format='raw').execute()
-msg_str = base64.urlsafe_b64decode(message['raw'].replace('-_', '+/').encode('ASCII'))
-mime_msg = email.message_from_bytes(msg_str)
-messageMainType = mime_msg.get_content_maintype()
-if messageMainType == 'multipart':
-    for part in mime_msg.get_payload():
-        print(part.get)
-        if part.get_content_maintype() == 'text':
-            output = part.get_payload()
-    return ""
-elif messageMainType == 'text':
-    return mime_msg.get_payload()
